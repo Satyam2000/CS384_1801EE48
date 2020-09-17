bb978c4e382f025eb87c002e2efbe04fc0b5a583
Satyam2000 <satyambgs10@gmail.com>
2020-09-17 15:53:27 +0530

    Hey,I have succesfully added power function in my Basic Python calculator!

--- Assignment1/test_cases_tutorial01.py
+++ Assignment1/test_cases_tutorial01.py
@@ -1,6 +1,6 @@
 import tutorial01 as A1
 
-actual_answers = [9, 12, 80, 5.0]
+actual_answers = [9, 12, 80, 5, 0.826, [2, 6, 18, 54, 162]]
 student_answers = []
 
 test_case_1 = A1.add(4, 5)
@@ -9,12 +9,27 @@ student_answers.append(test_case_1)
 test_case_2 = A1.subtract(14, 2)
 student_answers.append(test_case_2)
 
+
 test_case_3 = A1.multiply(10, 8)
 student_answers.append(test_case_3)
 
 test_case_4 = A1.divide(10, 2)
 student_answers.append(test_case_4)
 
+test_case_5 = A1.power(1.1, -2)
+student_answers.append(test_case_5)
+
+# Driver code 
+
+a = 2 # starting number 
+r = 3 # Common ratio 
+n = 5 # N th term to be find 
+
+gp = A1.printGP(a, r, n) 
+gp = list(gp) 
+student_answers.append(gp)
+
+print(gp)
 print(actual_answers)
 print(student_answers)
 
--- Assignment1/tutorial01.py
+++ Assignment1/tutorial01.py
@@ -1,22 +1,90 @@
 # Function to add two numbers 
 def add(num1, num2): 
-	addition = num1 + num2
-	return addition
+
+	if(not isinstance(num1,int)) and (not isinstance(num2,int)) :
+		return  0
+
+	else:
+		addition = num1 + num2
+		return addition
 
 # Function to subtract two numbers 
 def subtract(num1, num2): 
-	subtraction = num1 - num2
-	return subtraction
+	if(not isinstance(num1,int)) and (not isinstance(num2,int)) :
+		return  0
+
+	else :
+		subtraction = num1 - num2
+		return subtraction
 
 # Function to multiply two numbers 
 def multiply(num1, num2): 
 	#Multiplication Logic 
-	multiplication = num1 * num2
-	return multiplication
+
+	if(not isinstance(num1,int)) and (not isinstance(num2,int)) :
+		return  0
+	
+	else:
+		multiplication = num1 * num2
+		return multiplication
 
 # Function to divide two numbers 
 def divide(num1, num2): 
 	#DivisionLogic 
-	division = num1/num2
-	return division
+	if(not isinstance(num1,int)) and (not isinstance(num2,int)) :
+		return  0
 	
+	elif (num2==0):
+		return 0
+
+	else:	
+		division = num1/num2
+		return division
+		
+# Function to add power function
+#You cant use the inbuilt python function x ** y . Write your own function
+def power(base, exp): #base ^ exp
+	#Power Logic 
+	if(not isinstance(exp,int)):
+		return 0
+
+	elif(not isinstance(base,float)) and (not isinstance(base,int)) :
+		return  0
+
+	elif(base==0):
+		return 0	
+
+	elif (base==1):
+		return 1
+
+	elif (exp==0) :
+		return 1
+	
+	elif (base!=0) and (exp<0):
+		return  round(1/power(base, -exp),3)
+
+	elif(exp==1):
+		return(base)
+	
+	if(exp!=1) :
+		return round((base * power(base, exp-1)), 3)
+
+	
+# Python 3 program to print GP.  geometric Progression
+#You cant use the inbuilt python function. Write your own function
+def printGP(a, r, n): 
+	gp=[]
+	return gp 
+
+# Python 3 program to print AP.  arithmetic Progression
+#You cant use the inbuilt python function. Write your own function
+def printAP(a, d, n): 
+	ap=[]
+	return ap
+
+# Python 3 program to print HP.   Harmonic Progression
+#You cant use the inbuilt python function. Write your own function
+def printHP(a, d, n): 
+	hp=[]
+	return hp
+
