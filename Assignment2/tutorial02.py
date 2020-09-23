e789954a4a60f8b935c196ba57857f1f44e394d1
Satyam2000 <satyambgs10@gmail.com>
2020-09-24 00:28:12 +0530

    Hey! In my Python Calculator,I have added the mean and summation function

---
+++ Assignment2/tutorial02.py
@@ -0,0 +1,87 @@
+import math
+# All decimal 3 places
+
+# Function to compute mean
+def mean(first_list):
+    # mean Logic
+    mean_value = summation(first_list) / len(first_list)     
+
+    return round(mean_value,6)
+
+
+# Function to compute median. You cant use Python functions
+def median(first_list):
+    # median Logic
+    return median_value
+
+
+# Function to compute Standard deviation. You cant use Python functions
+def standard_deviation(first_list):
+    # Standard deviation Logic
+    return standard_deviation_value
+
+
+# Function to compute variance. You cant use Python functions
+def variance(first_list):
+    # variance Logic
+    return variance_value
+
+
+# Function to compute RMSE. You cant use Python functions
+def rmse(first_list, second_list):
+    # RMSE Logic
+    return rmse_value
+
+
+# Function to compute mse. You cant use Python functions
+def mse(first_list, second_list):
+    # mse Logic
+    return mse_value
+
+
+# Function to compute mae. You cant use Python functions
+def mae(first_list, second_list):
+    # mae Logic
+    return mae_value
+
+
+# Function to compute NSE. You cant use Python functions
+def nse(first_list, second_list):
+    # nse Logic
+    return nse_value
+
+
+# Function to compute Pearson correlation coefficient. You cant use Python functions
+def pcc(first_list, second_list):
+    # nse Logic
+    return pcc_value
+
+
+# Function to compute Skewness. You cant use Python functions
+def skewness(first_list):
+    # Skewness Logic
+    return skewness_value
+    
+def sorting(first_list):
+    # Sorting Logic
+    return sorted_list
+
+
+# Function to compute Kurtosis. You cant use Python functions
+def kurtosis(first_list):
+    # Kurtosis Logic
+    return kurtosis_value
+
+
+# Function to compute sum. You cant use Python functions
+def summation(first_list):
+    # sum Logic
+    summation_value = 0
+    for x in range(len(first_list)) :
+        if (not isinstance(first_list[x],int)) and (not isinstance(first_list[x],float)) :
+            return 0
+            
+        else:
+            summation_value = summation_value + first_list[x]
+
+    return summation_value
