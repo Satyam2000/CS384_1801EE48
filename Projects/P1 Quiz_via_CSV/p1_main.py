fb9f4243473690c82f69731251b304aa47bd6a1e
Satyam2000 <satyambgs10@gmail.com>
2020-12-05 01:07:29 +0530

    Implemented Various Utility function to export,read and write Databasel

--- Projects/P1 Quiz_via_CSV/p1_main.py
+++ Projects/P1 Quiz_via_CSV/p1_main.py
@@ -70,7 +70,7 @@ def timer(quiz):
         root.update()
         time.sleep(1)
         duration -= 1
-        
+
 def load_reg_data():
     users_data = []
     db_connection = sqlite3.connect("project1 quiz cs384.db")
@@ -145,3 +145,100 @@ def register_user():
     db_connection.close()
     print("You have Successfully registered!")
     return (name, roll_no, hashed_pw, contact_no)
+
+def show_user_data(user_data, skipped):
+    os.system('cls' if os.name =='nt'else 'clear')
+    print(f"Roll: {user_data[1]}\nName: {user_data[0]}")
+    if skipped:
+    	print(f"The Unattempted Questions: ", skipped_questions)
+    else:
+    	print(f"Unattempted Questions: ")
+    print("Goto Question: Press Ctrl+Alt+G")
+    print("Final Submit: Press Ctrl+Alt+F")
+    print("Export Database into CSV: Press Ctrl+Alt+E")
+    print()
+
+def store_marks_in_database(roll_no, quiz_no, marks):
+    os.chdir(current_path)
+    db_connection = sqlite3.connect('project1 quiz cs384.db')
+    curs = db_connection.cursor()
+
+    try:
+        curs.execute(
+            """CREATE TABLE project1_marks
+	    	         (roll text, quiz_no integer(2), marks integer(3))"""
+        )
+    except:
+        pass
+
+    if not type(marks) == int:
+    	mark = marks.item()
+    else:
+    	mark = marks
+    data = (roll_no, quiz_no)
+    curs.execute("DELETE FROM project1_marks WHERE roll=? AND quiz_no=?", data)
+    curs.execute("INSERT INTO project1_marks VALUES (?,?,?)",
+              (roll_no, quiz_no, mark))
+    db_connection.commit()
+    db_connection.close()
+
+
+
+def pressed_hotkeys():
+    tmp = ''
+    while is_keypress:
+        try:
+            if keyboard.is_pressed('ctrl+alt+u') or keyboard.is_pressed('ctrl+alt+U'):
+                tmp = 'skip_question'
+                return tmp
+            elif keyboard.is_pressed('ctrl+alt+e') or keyboard.is_pressed('ctrl+alt+E'):
+                tmp = 'export_to_csv'
+                return tmp
+            elif keyboard.is_pressed('ctrl+alt+g') or keyboard.is_pressed('ctrl+alt+G'):
+                tmp = 'goto_question'
+                return tmp
+            elif keyboard.is_pressed('ctrl+alt+f') or keyboard.is_pressed('ctrl+alt+F'):
+                end_timer = True
+                tmp = 'final_submit'
+                return tmp
+            elif keyboard.is_pressed('1'):
+            	tmp = '1'
+            	print("1")
+            	return tmp
+            elif keyboard.is_pressed('2'):
+            	tmp = '2'
+            	print("2")
+            	return tmp
+            elif keyboard.is_pressed('3'):
+            	tmp = '3'
+            	print("3")
+            	return tmp
+            elif keyboard.is_pressed('4'):
+            	tmp = '4'
+            	print("4")
+            	return tmp
+            elif keyboard.is_pressed('ctrl+q') or keyboard.is_pressed('ctrl+Q'):
+            	tmp = 'q'
+            	return tmp
+            elif keyboard.is_pressed('s') or keyboard.is_pressed('S'):
+            	tmp = 's'
+            	print("s")
+            	return tmp
+        except:
+            print("an error occured")
+            break
+
+def export_to_csv():
+	for i in range(1,4):
+		fileName = f"quiz{i}.csv"
+		db_link = sqlite3.connect('project1 quiz cs384.db')
+		c = db_link.cursor()
+		data_to_write = []
+		for row in c.execute("SELECT * FROM project1_marks WHERE quiz_no=?", (i,)):
+			data_to_write.append(row)
+		if not data_to_write == []:
+			with open(os.path.join(current_path,"quiz_wise_responses",fileName),mode= "w") as file:
+				writer = csv.writer(file)
+                # Writing the descriptive header
+				writer.writerow(["Roll No.", "Quiz No.", "Marks"])
+				writer.writerows(data_to_write)
