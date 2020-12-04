26acd9279df3bf7e51360e0500380e395a8c75b1
Satyam2000 <satyambgs10@gmail.com>
2020-12-05 01:41:19 +0530

    Hey,Now i successfully made notepad using tkinter in python.

--- Projects/P2 Notepad_GUI/p2_main.py
+++ Projects/P2 Notepad_GUI/p2_main.py
@@ -530,3 +530,71 @@ class Replace(tk.Toplevel):
             self.text.delete(idx, lastidx)
             self.text.insert(idx, new_term)
 
+
+class StatusBar(tk.Frame):
+    """Status bar that shows text widget statistics"""
+
+    def __init__(self, master, text_widget):
+        super().__init__(master, relief=tk.SUNKEN, bd=1)
+        self.master = master
+        self.text = text_widget
+        ttk.Separator(self).pack(fill=tk.X, expand=tk.YES)
+
+		#how to find Column Index and show in status bar
+        self.col_var = tk.StringVar()
+        self.col_var.set('0')
+        tk.Label(self, text='Col:', anchor=tk.W).pack(side=tk.LEFT)
+        tk.Label(self, textvariable=self.col_var,
+                 anchor=tk.W).pack(side=tk.LEFT)
+
+        #code to get Line Index
+        self.line_var = tk.StringVar()
+        self.line_var.set('1')
+        tk.Label(self, text='Line:', anchor=tk.W).pack(side=tk.LEFT)
+        tk.Label(self, textvariable=self.line_var,
+                 anchor=tk.W).pack(side=tk.LEFT)
+
+        # how to do Character Count and show in status bar
+        self.char_var = tk.StringVar()
+        self.char_var.set('0')
+        tk.Label(self, text='Chars:', anchor=tk.W).pack(side=tk.LEFT)
+        tk.Label(self, textvariable=self.char_var,
+                 anchor=tk.W).pack(side=tk.LEFT)
+
+        #how to find Word Count and show in status bar
+        self.word_var = tk.StringVar()
+        self.word_var.set('0')
+        tk.Label(self, text='Words:', anchor=tk.W).pack(side=tk.LEFT)
+        tk.Label(self, textvariable=self.word_var,
+                 anchor=tk.W).pack(side=tk.LEFT)
+
+        # code for Event binding
+        self.text.bind("<KeyRelease>", self.update_status)
+        self.text.bind("<ButtonRelease-1>", self.update_status)
+        self.update_status()  # set initial status
+        self.pack(side=tk.BOTTOM, fill=tk.X, padx=2, pady=2)
+
+    def update_status(self, event=None):
+        """Update Status Bar"""
+        line, col = self.text.index(tk.INSERT).split('.')
+        self.line_var.set(line)
+        self.col_var.set(col)
+
+        raw_text = self.text.get('1.0', tk.END)
+        spaces = raw_text.count(' ')
+
+        char_count = "{:,d}".format(len(raw_text)-spaces)
+        self.char_var.set(char_count)
+
+        word_list = [char for char in raw_text.split(
+            ' ') if char not in (' ', '', '\n')]
+        word_count = "{:,d}".format(len(word_list))
+        self.word_var.set(word_count)
+
+
+
+
+
+if __name__ == '__main__':
+    notepad = Notepad()
+    notepad.mainloop()
